b37444b74e69ecdd8c182e0371571298dd3a2775
tako <javadava.58.pts@gmail.com>
2020-11-28 23:53:09 +0900

    first commit

---
+++ javascript/section1/js/basic.js
@@ -0,0 +1,29 @@
+const array = [
+  ["a", "b", "c"],
+  [1, 2, 3],
+];
+console.log(array);
+
+// console
+//
+// (2) [Array(3), Array(3)]
+// 0: (3) ["a", "b", "c"]
+// 1: (3) [1, 2, 3]
+// length: 2
+// __proto__: Array(0)
+
+// (2) [Array(3), Array(3)]
+// 0: Array(3)
+// 0: "a"
+// 1: "b"
+// 2: "c"
+// length: 3
+// __proto__: Array(0)
+// 1: Array(3)
+// 0: 1
+// 1: 2
+// 2: 3
+// length: 3
+// __proto__: Array(0)
+// length: 2
+// __proto__: Array(0)
