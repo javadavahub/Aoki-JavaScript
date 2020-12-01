a9c42f909286a294279f9276356fa56715490f1a
tako <javadava.58.pts@gmail.com>
2020-12-01 23:48:38 +0900

    stringObject.js追加

--- javascript/section1/js/stringObject.js
+++ javascript/section1/js/stringObject.js
@@ -4,3 +4,42 @@ const test = "テスト";
 
 console.log(typeof test);
 console.log(test.length);
+
+/**
+ * 実行結果
+ * js % node stringObject.js
+ * string
+ * 3
+ */
+
+const test_2 = "文字列の結合";
+
+console.log(test_2.concat(test));
+
+/**
+ * 実行結果
+ * %
+ * 文字列結合テスト
+ */
+
+const greeting = "  Hello World ";
+
+console.log(greeting);
+console.log(greeting.trim());
+console.log(greeting);
+/**
+ * 実行結果
+ *
+ *   Hello World
+ * Hello world
+ *   Hello World
+ */
+
+console.log(greeting.replace("World", "JavaScript"));
+console.log(greeting);
+/**
+ * 実行結果
+ *
+ *   Hello JavaScript
+ *   Hello World
+ */
